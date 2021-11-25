d3456ab5101c157b93ddadbb2f87b13914001a00
Hari25483 <haricodmw@gmail.com>
2021-11-26 00:31:14 +0530

    Update Two_servo_motors.py

--- Two_servo_motors.py
+++ Two_servo_motors.py
@@ -1,35 +1,49 @@
-# Python Script
-# https://www.electronicshub.org/raspberry-pi-l298n-interface-tutorial-control-dc-motor-l298n-raspberry-pi/
+######################################################################################################################
+#
+#     Author : Hariharan Raveenthiran
+#     Prototype function for changing speed of motor(AC) by inputting no. of persons
+#
+######################################################################################################################
+
 
 import RPi.GPIO as GPIO          
 from time import sleep
 
+
 in1 = 24
 in2 = 23
 en = 25
-temp1=1
 
+
+#If DIRECTION_SELECTOR=1 motor will move forward. Else it moves backwards.
+DIRECTION_SELECTOR=1
+
+
+#Setting up the two servo motors
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
 GPIO.setup(en,GPIO.OUT)
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
+
+
+#Set the PWM frequency
 p=GPIO.PWM(en,1000)
 
 p.start(25)
 print("\n")
-print("The default speed & direction of motor is LOW & Forward.....")
-print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
-print("\n")    
+print("Instructions or commans are displayed below.")
+print("r-Run s-Stop f-Forward b-Backward l-Low m-Medium h-High e-Exit")
+    
 
-while(1):
+while(True):
 
     x=int(input("Enter no of persons"))
     
-    if x=='r':
-        print("run")
-        if(temp1==1):
+    if (x=='r'):
+        print("Run")
+        if(DIRECTION_SELECTOR==1):
          GPIO.output(in1,GPIO.HIGH)
          GPIO.output(in2,GPIO.LOW)
          print("forward")
@@ -47,9 +61,9 @@ while(1):
         GPIO.output(in2,GPIO.LOW)
         x='z'
         
-    p.ChangeDutyCycle(25)
-    
-    
+        
     else:
-        print("<<<  wrong data  >>>")
-        print("please enter the defined data to continue.....")
+        print("Enter a valid command:")
+
+
+    p.ChangeDutyCycle(25)
